4d05ae8013a898f2ed50b0b520fbf68f4e512cfd
bellday <92672351+bellday@users.noreply.github.com>
2023-05-15 17:39:40 +0900

    Create 서울에서김서방찾기.py

---
+++ programmers/lv1/서울에서김서방찾기.py
@@ -0,0 +1,17 @@
+def solution(seoul):
+
+    string=''
+
+    count =0
+
+    for s in seoul:
+
+        if s == "Kim":
+
+            
+
+            return '김서방은 '+ str(count) + '에 있다'
+
+        else:
+
+            count+=1
